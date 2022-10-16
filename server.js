369aaf4b78808a9b8f97439d515af9f5e94b8518
Gabriel Puente <gapuente96@gmail.com>
2022-10-16 00:19:35 -0300

    ajuste diretorio index para o heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/puc-pnjsol-front-angular'));
+app.use(express.static(__dirname + '/dist/puc-pnjsol-angular'));
 
 app.get('/*', (req,res) => {
-    res.sendFile(__dirname + '/dist/puc-pnjsol-front-angular/index.html');
+    res.sendFile(__dirname + '/dist/puc-pnjsol-angular/index.html');
 });
 
 app.listen(PORT, () =>{
